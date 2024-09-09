6894acc976bd9d79c472bf53dead13d86206133e
Thomas Berkane <thomas@berkane.fr>
2024-09-09 12:12:35 -0400

    increase model size

--- config/train_enwik8_char.py
+++ config/train_enwik8_char.py
@@ -1,8 +1,10 @@
+import time
+
 # train a miniature character-level enwik8 model
 # good for debugging and playing on macbooks and such
 
 out_dir = "out-enwik8-char"
-eval_interval = 250  # keep frequent because we'll overfit
+eval_interval = 500  # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10  # don't print too too often
 
@@ -11,7 +13,7 @@ always_save_checkpoint = False
 
 wandb_log = False  # override via command line if you like
 wandb_project = "enwik8"
-wandb_run_name = "mini-gpt"
+wandb_run_name = "run" + str(time.time())
 
 # these make the total batch size be ~60k
 # 12 batch size * 1024 block size * 5 gradaccum= 61,440
@@ -23,15 +25,14 @@ batch_size = 12
 block_size = 1024  # context of up to 1024 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+n_layer = 9
+n_head = 9
+n_embd = 576
+dropout = 0.0
 
-learning_rate = 1e-3  # with baby networks can afford to go a bit higher
+learning_rate = 8e-4  # with baby networks can afford to go a bit higher
 max_iters = 15000
 lr_decay_iters = 15000  # make equal to max_iters usually
-min_lr = 1e-4  # learning_rate / 10 usually
-beta2 = 0.99  # make a bit bigger because number of tokens per iter is small
+min_lr = 8e-5  # learning_rate / 10 usually
 
-warmup_iters = 100  # not super necessary potentially
+warmup_iters = 500  # not super necessary potentially
